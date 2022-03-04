c61cda4d90dd86cad8511f37c906cffd913d83a2
-abinsinguza-dallen <dallenabinsinguza@gmail.com>
2022-03-04 08:13:25 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,31 @@
+fun main (){
+    var text = "akirachix"
+    println(text[0].toString()+text[2]+text[3])
+    var m =sentence("dallen",20)
+    println(m)
+    var d = word()
+    println(d)
+    identity("DALLEN")
+    identity("SPERIA")
+}
+
+fun sentence(name: String , years: Int) :String{
+    var f =("Hi, my is $name and i am $years years old")
+    return(f)
+
+}
+fun word():Int{
+    var word ="success"
+    var l = word.length
+    return l
+}
+
+fun identity(name:String){
+    if(name=="DALLEN")
+        println("thats me")
+    else
+        println("i dont know who that is")
+}
+
+
+
